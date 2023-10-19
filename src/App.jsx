1e5387fedafef4f5555f9de9adc8fc5ee6f01534
ntp37 <nithipan3794@gmail.com>
2023-10-19 23:43:47 +0700

    update UI and fix responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
+import { MdNoteAdd, MdEditDocument } from "react-icons/md";
+import { IconContext } from "react-icons";
 import TaskList from "./components/TaskList";
 import "./App.css";
 
-function App() {
+export default function App() {
   const [data, setData] = useState("");
   const [indexTask, setIndexTask] = useState(null);
   const [todos, setTodos] = useState(() => {
@@ -11,21 +13,23 @@ function App() {
     return parseData || [];
   });
 
-  const addTask = () => {
+  const handleAddTask = () => {
     let todosArray = [...todos];
 
-    // For update task. Read state from type of the "indexTask" is a "number"
-    if (typeof indexTask === "number") {
-      todosArray[indexTask].taskTodo = data;
-      setIndexTask(null);
+    if (data) {
+      // For update task. Read state from type of the "indexTask" is a "number"
+      if (typeof indexTask === "number") {
+        todosArray[indexTask].taskTodo = data;
+        setIndexTask(null);
 
-      // For add new task.
-    } else {
-      todosArray.push({ taskTodo: data, taskState: false });
-      window.location.reload();
+        // For add new task.
+      } else {
+        todosArray.push({ taskTodo: data, taskState: false });
+        window.location.reload();
+      }
+      localStorage.setItem("taskData", JSON.stringify(todosArray));
+      setData("");
     }
-    localStorage.setItem("taskData", JSON.stringify(todosArray));
-    setData("");
   };
 
   const handleDelete = (index) => {
@@ -48,58 +52,80 @@ function App() {
     localStorage.setItem("taskData", JSON.stringify(todosArray));
   };
 
-  // const btnClassName =
+  const buttonText = () => {
+    if (typeof indexTask === "number") {
+      return (
+        <>
+          <MdEditDocument /><span>UPDATE</span>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <MdNoteAdd /><span>ADD</span>
+        </>
+      );
+    }
+  };
 
   return (
     <>
-      <div className="max-w-full min-h-screen bg-slate-100">
-        <div className="container grid mx-auto p-8">
-          <div className="text-center text-2xl font-medium">
-            <h1>TO-DO List</h1>
-          </div>
-          <div className="input-text flex gap-3 my-5 w-full">
-            <input
-              onChange={(e) => setData(e.target.value)}
-              value={data}
-              placeholder="Insert your task"
-              type="text"
-              className="h-10 shadow-lg p-3 w-11/12"
-            />
-            <button
-              className={`shadow-lg rounded-md w-1/12 text-white ${
-                typeof indexTask === "number"
-                  ? "bg-indigo-500 hover:bg-indigo-600"
-                  : "bg-emerald-500 hover:bg-emerald-600"
-              }`}
-              onClick={addTask}
-            >
-              {typeof indexTask === "number" ? "UPDATE" : "ADD"}
-            </button>
-          </div>
-          <div className=" flex flex-row gap-4 justify-center my-3">
-            <div className="inline-flex items-center gap-2">
-              <div className="w-3 h-3 bg-lime-500"></div>
-              <span>Completed</span>
+      <div className="w-screen min-h-screen bg-slate-200 font-inter">
+        <IconContext.Provider value={{ className: "text-xl" }}>
+          <div className="container flex flex-col mx-auto p-4 lg:p-7">
+            <div className="my-6 text-center text-2xl font-semibold">
+              <h1>TO-DO List</h1>
             </div>
-            <div className="inline-flex items-center gap-2">
-              <span>Incomplete</span>
-              <div className="w-3 h-3 bg-slate-50 border border-black"></div>
+            <div className="input-task sm:relative sm:flex-row sm:gap-3 flex flex-col gap-5 my-5">
+              <input
+                type="text"
+                value={data}
+                placeholder="Insert your task"
+                className="sm:text-base sm:h-14 h-12 p-3 text-sm shadow-lg w-full rounded-full border focus-visible:outline-none
+                  focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1"
+                onChange={(e) => setData(e.target.value)}
+              />
+              <button
+                onClick={handleAddTask}
+                className={`sm:absolute sm:m-2 sm:right-0 sm:h-10 sm:text-base self-center w-32 h-9 font-medium text-sm shadow-lg
+                  rounded-full text-white duration-300 hover:scale-105 ${
+                    typeof indexTask === "number"
+                      ? "bg-indigo-500 hover:bg-indigo-600"
+                      : "bg-emerald-500 hover:bg-emerald-600"
+                  }`}
+              >
+                <div className="flex gap-1 justify-center items-center">
+                  {buttonText()}
+                </div>
+              </button>
+            </div>
+            <div
+              className={`${!todos.length ? "hidden" : "flex"} sm:text-base gap-6 justify-center my-3 text-sm font-medium`}>
+              <div className="inline-flex items-center gap-2">
+                <div className="w-3 h-3 bg-lime-500"></div>
+                <span>Completed Tasks</span>
+              </div>
+              <div className="inline-flex items-center gap-2">
+                <span>Active Tasks</span>
+                <div className="w-3 h-3 bg-slate-50 border border-black"></div>
+              </div>
+            </div>
+            <div className="tasks overflow-hidden">
+              {todos?.map((task, index) => (
+                <TaskList
+                  key={index}
+                  index={index}
+                  indexTask={indexTask}
+                  dataTask={task}
+                  handleDelete={handleDelete}
+                  handleEdit={handleEdit}
+                  taskCompleted={taskCompleted}
+                />
+              ))}
             </div>
           </div>
-          {todos?.map((task, index) => (
-            <TaskList
-              key={index}
-              dataTask={task}
-              index={index}
-              handleDelete={handleDelete}
-              handleEdit={handleEdit}
-              taskCompleted={taskCompleted}
-            />
-          ))}
-        </div>
+        </IconContext.Provider>
       </div>
     </>
   );
 }
-
-export default App;
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,16 +1,37 @@
-export default function TaskList({ dataTask, index, handleDelete, handleEdit, taskCompleted }) {
+import { AiTwotoneEdit, AiTwotoneDelete } from "react-icons/ai";
+
+export default function TaskList({ dataTask, index, handleDelete, handleEdit, taskCompleted, indexTask }) {
   return (
     <>
-        <div id={`task-${index}`} className="listItem mt-3 p-4 flex w-full h-16 shadow-xl bg-slate-50 items-center justify-between" style={ dataTask.taskState ? {backgroundColor: "#84cc16"} : {backgroundColor: ""}}>
-          <div className="flex gap-4">
-            <input type="checkbox" className="h-5 w-5 self-center" onClick={() => taskCompleted(index)} defaultChecked={dataTask.taskState} />
-            <p>{dataTask.taskTodo}</p>
-          </div>
-          <div className="btn-group flex gap-2">
-            <button className="bg-blue-500 hover:bg-blue-600 shadow-lg rounded-md w-24 h-8 text-white" onClick={() => handleEdit(index)}>EDIT</button>
-            <button className="bg-red-500 hover:bg-red-600 shadow-lg rounded-md w-24 h-8 text-white" onClick={() => handleDelete(index)}>DELETE</button>
-          </div>
+      <div
+        id={`task-${index}`}
+        className="taskList duration-300 rounded-lg mt-3 p-2 sm:p-4 flex w-full h-auto shadow-xl bg-slate-50 items-center justify-between hover:bg-orange-100"
+        style={dataTask.taskState ? { backgroundColor: "#84cc16" } : { backgroundColor: "" }}
+      >
+        <div className="flex gap-4 text-sm sm:text-base overflow-hidden">
+          <input type="checkbox" className="h-4 w-4 sm:h-5 sm:w-5 self-center" onClick={() => taskCompleted(index)} defaultChecked={dataTask.taskState} />
+          <p className="break-words overflow-hidden p-2">{dataTask.taskTodo}</p>
         </div>
+        <div className="btn-group flex gap-2 font-medium">
+          <button
+            className="sm:text-base sm:w-20 md:w-24 sm:h-9 w-8 h-7 duration-300 shadow-lg rounded-lg text-sm bg-blue-500 hover:bg-blue-600 hover:scale-105 text-white"
+            onClick={() => handleEdit(index)}
+          >
+            <div className="flex gap-1 justify-center items-center">
+              <AiTwotoneEdit /><span className="hidden sm:inline">Edit</span>
+            </div>
+          </button>
+          <button
+            className="sm:text-base sm:w-20 md:w-24 sm:h-9 w-8 h-7 duration-300 shadow-lg rounded-lg text-sm bg-red-500 hover:bg-red-600 hover:scale-105 text-white"
+            onClick={() => handleDelete(index)}
+            disabled={indexTask === index ? true : false}
+          >
+            <div className="flex gap-1 justify-center items-center">
+              <AiTwotoneDelete /><span className="hidden sm:inline">Delete</span>
+            </div>
+          </button>
+        </div>
+      </div>
     </>
   );
 }
